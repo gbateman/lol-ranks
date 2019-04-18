616a4966be9a635dcce312a50922a352c06cdf1a
Greg Bateman <gerbot150@gmail.com>
2019-04-18 01:21:08 -0700

    Set up firestore db

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('/etc/lol-ranks/gregbateman-me-247fe3ed8a98.json');
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+const fs = require('fs');
+fs.readFile('./summoners.json', (err, data) => {
+    if (err) { throw err; }
+    const summonerIDs = JSON.parse(data.toString());
+    console.log('summonerIDs: ', summonerIDs);
+});
