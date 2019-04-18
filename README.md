9685d611a9c4c32a6a85d9cae215b7a84ae61534
Greg Bateman <gerbot150@gmail.com>
2019-04-18 01:47:52 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # lol-ranks
+
+cert:
+`/etc/lol-ranks/lol-ranks.json`
+
+env vars:
+`RIOT_API_TOKEN`: api token used to query the riot api
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 const fs = require('fs');
 const request = require('request');
-const serviceAccount = require('/etc/lol-ranks/gregbateman-me-247fe3ed8a98.json');
+const serviceAccount = require('/etc/lol-ranks/lol-ranks.json');
 const riotAPIToken = process.env.RIOT_API_TOKEN || '';
 const soloQueue = 'RANKED_SOLO_5x5';
 
